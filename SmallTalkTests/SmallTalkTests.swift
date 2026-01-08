08c81b229495285529fc80bc849590c3315c1a9c
meetminh <143404202+meetminh@users.noreply.github.com>
2026-01-08 13:37:06 -0500

    Initial Commit

---
+++ SmallTalkTests/SmallTalkTests.swift
@@ -0,0 +1,16 @@
+//
+//  SmallTalkTests.swift
+//  SmallTalkTests
+//
+//  Created by QuangMinh Tran on 08.01.26.
+//
+
+import Testing
+
+struct SmallTalkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
